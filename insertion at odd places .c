5bef45c5f27b565b450b257e4cae988fc0f413e8
Barneel Ray <98656468+BarneelRay@users.noreply.github.com>
2022-03-21 22:55:13 +0530

    Create insertion at odd places .c

---
+++ insertion at odd places .c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//insertion of elements only at positions with both odd indexes in 2 d array.
+int main(){
+   /* 2D array declaration*/
+   int disp[2][3]={{0}};
+   /*Counter variables for the loop*/
+   int i, j;
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         if (i%2!=0&&j%2!=0){
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &disp[i][j]);
+         }
+      }
+   }
+   //Displaying array elements
+   printf("Two Dimensional array elements:\n");
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);
+         if(j==2){
+            printf("\n");
+         }
+      }
+   }
+   return 0;
+}
+
